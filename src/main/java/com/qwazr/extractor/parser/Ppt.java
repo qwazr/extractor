0e7063866b67c0eddd9adc0e20f0968c210ed402
Emmanuel Keller <ekeller@open-search-server.com>
2015-09-28 14:20:21 +0200

    Fix wrong dependencies :

--- src/main/java/com/qwazr/extractor/parser/Ppt.java
+++ src/main/java/com/qwazr/extractor/parser/Ppt.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,99 +16,101 @@
 package com.qwazr.extractor.parser;
 
 import java.io.InputStream;
+import java.util.List;
 
-import org.apache.poi.hslf.model.Slide;
-import org.apache.poi.hslf.model.TextRun;
 import org.apache.poi.hslf.record.TextHeaderAtom;
-import org.apache.poi.hslf.usermodel.SlideShow;
+import org.apache.poi.hslf.usermodel.HSLFSlide;
+import org.apache.poi.hslf.usermodel.HSLFSlideShow;
+import org.apache.poi.hslf.usermodel.HSLFTextParagraph;
 
 import com.qwazr.extractor.ParserAbstract;
 import com.qwazr.extractor.ParserDocument;
 import com.qwazr.extractor.ParserField;
+import org.apache.poi.hslf.usermodel.HSLFTextRun;
 
 public class Ppt extends ParserAbstract {
 
-	public static final String[] DEFAULT_MIMETYPES = { "application/vnd.ms-powerpoint" };
+    public static final String[] DEFAULT_MIMETYPES = { "application/vnd.ms-powerpoint" };
 
-	public static final String[] DEFAULT_EXTENSIONS = { "ppt" };
+    public static final String[] DEFAULT_EXTENSIONS = { "ppt" };
 
-	final protected static ParserField TITLE = ParserField.newString("title",
-			"The title of the document");
+    final protected static ParserField TITLE = ParserField.newString("title", "The title of the document");
 
-	final protected static ParserField BODY = ParserField.newString("body",
-			"The body of the document");
+    final protected static ParserField BODY = ParserField.newString("body", "The body of the document");
 
-	final protected static ParserField NOTES = ParserField.newString("notes",
-			null);
+    final protected static ParserField NOTES = ParserField.newString("notes", null);
 
-	final protected static ParserField OTHER = ParserField.newString("other",
-			null);
+    final protected static ParserField OTHER = ParserField.newString("other", null);
 
-	final protected static ParserField LANG_DETECTION = ParserField.newString(
-			"lang_detection", "Detection of the language");
+    final protected static ParserField LANG_DETECTION = ParserField
+		    .newString("lang_detection", "Detection of the language");
 
-	final protected static ParserField[] FIELDS = { TITLE, BODY, NOTES, OTHER,
-			LANG_DETECTION };
+    final protected static ParserField[] FIELDS = { TITLE, BODY, NOTES, OTHER, LANG_DETECTION };
 
-	public Ppt() {
-	}
+    public Ppt() {
+    }
 
-	@Override
-	protected ParserField[] getParameters() {
-		return null;
-	}
+    @Override
+    protected ParserField[] getParameters() {
+	return null;
+    }
 
-	@Override
-	protected ParserField[] getFields() {
-		return FIELDS;
-	}
+    @Override
+    protected ParserField[] getFields() {
+	return FIELDS;
+    }
 
-	@Override
-	protected String[] getDefaultExtensions() {
-		return DEFAULT_EXTENSIONS;
-	}
+    @Override
+    protected String[] getDefaultExtensions() {
+	return DEFAULT_EXTENSIONS;
+    }
 
-	@Override
-	protected String[] getDefaultMimeTypes() {
-		return DEFAULT_MIMETYPES;
-	}
+    @Override
+    protected String[] getDefaultMimeTypes() {
+	return DEFAULT_MIMETYPES;
+    }
 
-	@Override
-	protected void parseContent(InputStream inputStream, String extension,
-			String mimeType) throws Exception {
+    @Override
+    protected void parseContent(InputStream inputStream, String extension, String mimeType) throws Exception {
 
-		SlideShow ppt = new SlideShow(inputStream);
+	HSLFSlideShow ppt = new HSLFSlideShow(inputStream);
 
-		Slide[] slides = ppt.getSlides();
-		for (Slide slide : slides) {
-			ParserDocument document = getNewParserDocument();
-			TextRun[] textRuns = slide.getTextRuns();
-			for (TextRun textRun : textRuns) {
-				ParserField parserField;
-				switch (textRun.getRunType()) {
-				case TextHeaderAtom.TITLE_TYPE:
-				case TextHeaderAtom.CENTER_TITLE_TYPE:
-					parserField = TITLE;
-					break;
-				case TextHeaderAtom.NOTES_TYPE:
-					parserField = NOTES;
-					break;
-				case TextHeaderAtom.BODY_TYPE:
-				case TextHeaderAtom.CENTRE_BODY_TYPE:
-				case TextHeaderAtom.HALF_BODY_TYPE:
-				case TextHeaderAtom.QUARTER_BODY_TYPE:
-					parserField = BODY;
-					break;
-				case TextHeaderAtom.OTHER_TYPE:
-				default:
-					parserField = OTHER;
-					break;
-				}
-				document.add(parserField, textRun.getText());
-			}
-			document.add(LANG_DETECTION,
-					languageDetection(document, BODY, 10000));
+	List<HSLFSlide> slides = ppt.getSlides();
+	for (HSLFSlide slide : slides) {
+	    ParserDocument document = getNewParserDocument();
+	    List<List<HSLFTextParagraph>> textLevel0 = slide.getTextParagraphs();
+	    for (List<HSLFTextParagraph> textLevel1 : textLevel0) {
+		for (HSLFTextParagraph textPara : textLevel1) {
+		    ParserField parserField;
+		    switch (textPara.getRunType()) {
+		    case TextHeaderAtom.TITLE_TYPE:
+		    case TextHeaderAtom.CENTER_TITLE_TYPE:
+			parserField = TITLE;
+			break;
+		    case TextHeaderAtom.NOTES_TYPE:
+			parserField = NOTES;
+			break;
+		    case TextHeaderAtom.BODY_TYPE:
+		    case TextHeaderAtom.CENTRE_BODY_TYPE:
+		    case TextHeaderAtom.HALF_BODY_TYPE:
+		    case TextHeaderAtom.QUARTER_BODY_TYPE:
+			parserField = BODY;
+			break;
+		    case TextHeaderAtom.OTHER_TYPE:
+		    default:
+			parserField = OTHER;
+			break;
+		    }
+		    StringBuilder sb = new StringBuilder();
+		    for (HSLFTextRun textRun : textPara.getTextRuns()) {
+			sb.append(textRun.getRawText());
+			sb.append(' ');
+		    }
+		    document.add(parserField, sb.toString().trim());
 		}
-
+	    }
+	    document.add(LANG_DETECTION, languageDetection(document, BODY, 10000));
 	}
+
+    }
 }
--- src/main/java/com/qwazr/extractor/parser/Pptx.java
+++ src/main/java/com/qwazr/extractor/parser/Pptx.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@ package com.qwazr.extractor.parser;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.POIXMLProperties.CoreProperties;
@@ -42,201 +43,182 @@ import com.qwazr.extractor.ParserField;
 
 public class Pptx extends ParserAbstract {
 
-	public static final String[] DEFAULT_MIMETYPES = { "application/vnd.openxmlformats-officedocument.presentationml.presentation" };
+    public static final String[] DEFAULT_MIMETYPES = {
+		    "application/vnd.openxmlformats-officedocument.presentationml.presentation" };
 
-	public static final String[] DEFAULT_EXTENSIONS = { "pptx" };
+    public static final String[] DEFAULT_EXTENSIONS = { "pptx" };
 
-	final protected static ParserField TITLE = ParserField.newString("title",
-			"The title of the document");
+    final protected static ParserField TITLE = ParserField.newString("title", "The title of the document");
 
-	final protected static ParserField CREATOR = ParserField.newString(
-			"creator", "The name of the creator");
+    final protected static ParserField CREATOR = ParserField.newString("creator", "The name of the creator");
 
-	final protected static ParserField DESCRIPTION = ParserField.newString(
-			"description", null);
+    final protected static ParserField DESCRIPTION = ParserField.newString("description", null);
 
-	final protected static ParserField KEYWORDS = ParserField.newString(
-			"keywords", null);
+    final protected static ParserField KEYWORDS = ParserField.newString("keywords", null);
 
-	final protected static ParserField SUBJECT = ParserField.newString(
-			"subject", "The subject of the document");
+    final protected static ParserField SUBJECT = ParserField.newString("subject", "The subject of the document");
 
-	final protected static ParserField CREATION_DATE = ParserField.newDate(
-			"creation_date", null);
+    final protected static ParserField CREATION_DATE = ParserField.newDate("creation_date", null);
 
-	final protected static ParserField MODIFICATION_DATE = ParserField.newDate(
-			"modification_date", null);
+    final protected static ParserField MODIFICATION_DATE = ParserField.newDate("modification_date", null);
 
-	final protected static ParserField SLIDES = ParserField.newString("slides",
-			null);
+    final protected static ParserField SLIDES = ParserField.newString("slides", null);
 
-	final protected static ParserField MASTER = ParserField.newString("master",
-			null);
+    final protected static ParserField MASTER = ParserField.newString("master", null);
 
-	final protected static ParserField NOTES = ParserField.newString("notes",
-			null);
+    final protected static ParserField NOTES = ParserField.newString("notes", null);
 
-	final protected static ParserField COMMENTS = ParserField.newString(
-			"comments", null);
+    final protected static ParserField COMMENTS = ParserField.newString("comments", null);
 
-	final protected static ParserField LANG_DETECTION = ParserField.newString(
-			"lang_detection", "Detection of the language");
+    final protected static ParserField LANG_DETECTION = ParserField
+		    .newString("lang_detection", "Detection of the language");
 
-	final protected static ParserField[] FIELDS = { TITLE, CREATOR,
-			DESCRIPTION, KEYWORDS, SUBJECT, CREATION_DATE, MODIFICATION_DATE,
-			SLIDES, MASTER, NOTES, COMMENTS, LANG_DETECTION };
+    final protected static ParserField[] FIELDS = { TITLE, CREATOR, DESCRIPTION, KEYWORDS, SUBJECT, CREATION_DATE,
+		    MODIFICATION_DATE, SLIDES, MASTER, NOTES, COMMENTS, LANG_DETECTION };
 
-	public Pptx() {
+    public Pptx() {
+    }
+
+    @Override
+    protected ParserField[] getParameters() {
+	return null;
+    }
+
+    @Override
+    protected ParserField[] getFields() {
+	return FIELDS;
+    }
+
+    @Override
+    protected String[] getDefaultExtensions() {
+	return DEFAULT_EXTENSIONS;
+    }
+
+    @Override
+    protected String[] getDefaultMimeTypes() {
+	return DEFAULT_MIMETYPES;
+    }
+
+    @Override
+    protected void parseContent(InputStream inputStream, String extension, String mimeType) throws Exception {
+	if (StringUtils.isEmpty(extension))
+	    extension = ".pptx";
+	File tempFile = ParserAbstract.createTempFile(inputStream, extension);
+	try {
+	    parseContent(tempFile, extension, mimeType);
+	} finally {
+	    tempFile.delete();
 	}
+    }
 
-	@Override
-	protected ParserField[] getParameters() {
-		return null;
+    @Override
+    protected void parseContent(File file, String extension, String mimeType) throws Exception {
+
+	XSLFSlideShow pptSlideShow = new XSLFSlideShow(file.getAbsolutePath());
+	XMLSlideShow slideshow = new XMLSlideShow(pptSlideShow.getPackage());
+
+	// Extract metadata
+	XSLFPowerPointExtractor poiExtractor = null;
+	try {
+	    poiExtractor = new XSLFPowerPointExtractor(slideshow);
+	    CoreProperties info = poiExtractor.getCoreProperties();
+	    if (info != null) {
+		metas.add(TITLE, info.getTitle());
+		metas.add(CREATOR, info.getCreator());
+		metas.add(SUBJECT, info.getSubject());
+		metas.add(DESCRIPTION, info.getDescription());
+		metas.add(KEYWORDS, info.getKeywords());
+		metas.add(CREATION_DATE, info.getCreated());
+		metas.add(MODIFICATION_DATE, info.getModified());
+	    }
+	} finally {
+	    poiExtractor.close();
 	}
+	extractSides(slideshow);
+    }
 
-	@Override
-	protected ParserField[] getFields() {
-		return FIELDS;
-	}
-
-	@Override
-	protected String[] getDefaultExtensions() {
-		return DEFAULT_EXTENSIONS;
-	}
-
-	@Override
-	protected String[] getDefaultMimeTypes() {
-		return DEFAULT_MIMETYPES;
-	}
-
-	@Override
-	protected void parseContent(InputStream inputStream, String extension,
-			String mimeType) throws Exception {
-		if (StringUtils.isEmpty(extension))
-			extension = ".pptx";
-		File tempFile = ParserAbstract.createTempFile(inputStream, extension);
-		try {
-			parseContent(tempFile, extension, mimeType);
-		} finally {
-			tempFile.delete();
+    /**
+     * Declined from XSLFPowerPointExtractor.java
+     */
+    private String extractText(XSLFCommonSlideData data, boolean skipPlaceholders) {
+	StringBuilder sb = new StringBuilder();
+	for (DrawingTextBody textBody : data.getDrawingText()) {
+	    if (skipPlaceholders && textBody instanceof DrawingTextPlaceholder) {
+		DrawingTextPlaceholder ph = (DrawingTextPlaceholder) textBody;
+		if (!ph.isPlaceholderCustom()) {
+		    // Skip non-customised placeholder text
+		    continue;
 		}
+	    }
+
+	    for (DrawingParagraph p : textBody.getParagraphs()) {
+		sb.append(p.getText());
+		sb.append("\n");
+	    }
 	}
+	return sb.toString();
+    }
 
-	@Override
-	protected void parseContent(File file, String extension, String mimeType)
-			throws Exception {
+    /**
+     * Declined from XSLFPowerPointExtractor.java
+     *
+     * @param slideshow
+     */
+    private void extractSides(XMLSlideShow slideshow) {
 
-		XSLFSlideShow pptSlideShow = new XSLFSlideShow(file.getAbsolutePath());
-		XMLSlideShow slideshow = new XMLSlideShow(pptSlideShow.getPackage());
+	List<XSLFSlide> slides = slideshow.getSlides();
+	XSLFCommentAuthors commentAuthors = slideshow.getCommentAuthors();
 
-		// Extract metadata
-		XSLFPowerPointExtractor poiExtractor = null;
-		try {
-			poiExtractor = new XSLFPowerPointExtractor(slideshow);
-			CoreProperties info = poiExtractor.getCoreProperties();
-			if (info != null) {
-				metas.add(TITLE, info.getTitle());
-				metas.add(CREATOR, info.getCreator());
-				metas.add(SUBJECT, info.getSubject());
-				metas.add(DESCRIPTION, info.getDescription());
-				metas.add(KEYWORDS, info.getKeywords());
-				metas.add(CREATION_DATE, info.getCreated());
-				metas.add(MODIFICATION_DATE, info.getModified());
+	for (XSLFSlide slide : slides) {
+
+	    // One document per slide
+	    ParserDocument result = getNewParserDocument();
+
+	    XSLFNotes notes = slide.getNotes();
+	    XSLFComments comments = slide.getComments();
+	    XSLFSlideLayout layout = slide.getSlideLayout();
+	    XSLFSlideMaster master = layout.getSlideMaster();
+
+	    // TODO Do the slide's name
+	    // (Stored in docProps/app.xml)
+
+	    // Do the slide's text
+	    result.add(SLIDES, extractText(slide.getCommonSlideData(), false));
+	    result.add(LANG_DETECTION, languageDetection(SLIDES, 10000));
+
+	    // If requested, get text from the master and it's layout
+	    if (layout != null) {
+		result.add(MASTER, extractText(layout.getCommonSlideData(), true));
+	    }
+	    if (master != null) {
+		result.add(MASTER, extractText(master.getCommonSlideData(), true));
+	    }
+
+	    // If the slide has comments, do those too
+	    if (comments != null) {
+		for (CTComment comment : comments.getCTCommentsList().getCmList()) {
+		    StringBuilder sbComment = new StringBuilder();
+		    // Do the author if we can
+		    if (commentAuthors != null) {
+			CTCommentAuthor author = commentAuthors.getAuthorById(comment.getAuthorId());
+			if (author != null) {
+			    sbComment.append(author.getName());
+			    sbComment.append(": ");
 			}
-		} finally {
-			poiExtractor.close();
+		    }
+
+		    // Then the comment text, with a new line afterwards
+		    sbComment.append(comment.getText());
+		    sbComment.append("\n");
+		    if (sbComment.length() > 0)
+			result.add(COMMENTS, sbComment.toString());
 		}
-		extractSides(slideshow);
+	    }
+
+	    // Do the notes if requested
+	    if (notes != null) {
+		result.add(NOTES, extractText(notes.getCommonSlideData(), false));
+	    }
 	}
-
-	/**
-	 * Declined from XSLFPowerPointExtractor.java
-	 */
-	private String extractText(XSLFCommonSlideData data,
-			boolean skipPlaceholders) {
-		StringBuilder sb = new StringBuilder();
-		for (DrawingTextBody textBody : data.getDrawingText()) {
-			if (skipPlaceholders && textBody instanceof DrawingTextPlaceholder) {
-				DrawingTextPlaceholder ph = (DrawingTextPlaceholder) textBody;
-				if (!ph.isPlaceholderCustom()) {
-					// Skip non-customised placeholder text
-					continue;
-				}
-			}
-
-			for (DrawingParagraph p : textBody.getParagraphs()) {
-				sb.append(p.getText());
-				sb.append("\n");
-			}
-		}
-		return sb.toString();
-	}
-
-	/**
-	 * Declined from XSLFPowerPointExtractor.java
-	 * 
-	 * @param pptSlideShow
-	 */
-	private void extractSides(XMLSlideShow slideshow) {
-
-		XSLFSlide[] slides = (XSLFSlide[]) slideshow.getSlides();
-		XSLFCommentAuthors commentAuthors = slideshow.getCommentAuthors();
-
-		for (XSLFSlide slide : slides) {
-
-			// One document per slide
-			ParserDocument result = getNewParserDocument();
-
-			XSLFNotes notes = slide.getNotes();
-			XSLFComments comments = slide.getComments();
-			XSLFSlideLayout layout = slide.getSlideLayout();
-			XSLFSlideMaster master = layout.getSlideMaster();
-
-			// TODO Do the slide's name
-			// (Stored in docProps/app.xml)
-
-			// Do the slide's text
-			result.add(SLIDES, extractText(slide.getCommonSlideData(), false));
-			result.add(LANG_DETECTION, languageDetection(SLIDES, 10000));
-
-			// If requested, get text from the master and it's layout
-			if (layout != null) {
-				result.add(MASTER,
-						extractText(layout.getCommonSlideData(), true));
-			}
-			if (master != null) {
-				result.add(MASTER,
-						extractText(master.getCommonSlideData(), true));
-			}
-
-			// If the slide has comments, do those too
-			if (comments != null) {
-				for (CTComment comment : comments.getCTCommentsList()
-						.getCmList()) {
-					StringBuilder sbComment = new StringBuilder();
-					// Do the author if we can
-					if (commentAuthors != null) {
-						CTCommentAuthor author = commentAuthors
-								.getAuthorById(comment.getAuthorId());
-						if (author != null) {
-							sbComment.append(author.getName());
-							sbComment.append(": ");
-						}
-					}
-
-					// Then the comment text, with a new line afterwards
-					sbComment.append(comment.getText());
-					sbComment.append("\n");
-					if (sbComment.length() > 0)
-						result.add(COMMENTS, sbComment.toString());
-				}
-			}
-
-			// Do the notes if requested
-			if (notes != null) {
-				result.add(NOTES,
-						extractText(notes.getCommonSlideData(), false));
-			}
-		}
-	}
+    }
 }
