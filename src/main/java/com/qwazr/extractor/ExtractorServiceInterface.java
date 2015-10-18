44eeaacbdae8ee19de912d26b9d065aa43c5448c
Emmanuel Keller <ekeller@open-search-server.com>
2015-10-18 16:34:05 +0200

    Add realm

--- src/main/java/com/qwazr/extractor/ExtractorServiceInterface.java
+++ src/main/java/com/qwazr/extractor/ExtractorServiceInterface.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,22 +15,18 @@
  */
 package com.qwazr.extractor;
 
-import java.io.InputStream;
-import java.util.Map;
+import com.qwazr.utils.server.RestApplication;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
+import java.io.InputStream;
+import java.util.Map;
 
-import com.qwazr.utils.server.RestApplication;
-
+@RolesAllowed(ExtractorServer.SERVICE_NAME_EXTRACTOR)
 @Path("/extractor")
 public interface ExtractorServiceInterface {
 
@@ -42,26 +38,20 @@ public interface ExtractorServiceInterface {
 	@GET
 	@Path("/{name}")
 	@Produces(RestApplication.APPLICATION_JSON_UTF8)
-	public Object get(@Context UriInfo uriInfo,
-			@PathParam("name") String parserName,
-			@QueryParam("path") String path);
+	public Object get(@Context UriInfo uriInfo, @PathParam("name") String parserName, @QueryParam("path") String path);
 
 	@PUT
 	@Path("/{name}")
 	@Produces(RestApplication.APPLICATION_JSON_UTF8)
-	public ParserResult put(@Context UriInfo uriInfo,
-			@PathParam("name") String parserName,
-			@QueryParam("path") String filePath, InputStream inputStream);
+	public ParserResult put(@Context UriInfo uriInfo, @PathParam("name") String parserName,
+					@QueryParam("path") String filePath, InputStream inputStream);
 
-	public ParserResult extract(String parserName,
-			MultivaluedMap<String, String> parameters, String filePath,
-			InputStream inputStream);
+	public ParserResult extract(String parserName, MultivaluedMap<String, String> parameters, String filePath,
+					InputStream inputStream);
 
 	@PUT
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public ParserResult putMagic(@Context UriInfo uriInfo,
-			@QueryParam("name") String fileName,
-			@QueryParam("path") String filePath,
-			@QueryParam("type") String mimeType, InputStream inputStream);
+	public ParserResult putMagic(@Context UriInfo uriInfo, @QueryParam("name") String fileName,
+					@QueryParam("path") String filePath, @QueryParam("type") String mimeType, InputStream inputStream);
 }
