199be08693589135a7fc7296c15d8b50b6f4950e
Emmanuel Keller <ekeller@open-search-server.com>
2016-01-13 21:14:49 +0100

    Master/node refactorying

--- src/main/java/com/qwazr/extractor/ExtractorServer.java
+++ src/main/java/com/qwazr/extractor/ExtractorServer.java
@@ -28,11 +28,12 @@ import javax.servlet.ServletException;
 import javax.ws.rs.ApplicationPath;
 import java.io.IOException;
 import java.util.Set;
+import java.util.concurrent.Executors;
 
 public class ExtractorServer extends AbstractServer {
 
 	private ExtractorServer() {
-		super(new ServerDefinition());
+		super(new ServerDefinition(), Executors.newCachedThreadPool());
 	}
 
 	@ApplicationPath("/")
@@ -53,7 +54,7 @@ public class ExtractorServer extends AbstractServer {
 
 	@Override
 	public void load() throws IOException {
-		ClusterManager.load(getWebServicePublicAddress(), getCurrentDataDir());
+		ClusterManager.load(executorService, getWebServicePublicAddress(), null);
 		ParserManager.load();
 	}
 
